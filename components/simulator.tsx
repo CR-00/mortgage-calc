ea62e18eecb5f0edbb0b269e18ae4b246903bfa5
cr <CJR022@student.bham.ac.uk>
2025-01-25 18:13:34 +0000

    rm import

--- components/simulator.tsx
+++ components/simulator.tsx
@@ -1,4 +1,3 @@
-import * as math from 'mathjs';
 import { Button, Container, Group, Indicator, NumberInput, Stack, Text, Title } from "@mantine/core";
 import { useState } from "react";
 import { LineChart } from '@mantine/charts';
@@ -15,10 +14,9 @@ const Simulator = () => {
     const [interestRateStdDev, setInterestRateStdDev] = useState<number>(0.1);
 
     function gaussianRandom(mean=0, stdev=1) {
-        const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+        const u = 1 - Math.random();
         const v = Math.random();
         const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-        // Transform to the desired mean and standard deviation:
         return z * stdev + mean;
     }
 
